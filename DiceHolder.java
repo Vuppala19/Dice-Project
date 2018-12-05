ad70f15827907c40d4a1c354f1fd711b623709a1
Vuppala19 <sreeyavuppala@gmail.com>
2018-12-05 07:52:08 -0500

    addDie method created

--- DiceHolder.java
+++ DiceHolder.java
@@ -1,8 +1,22 @@
+import java.util.ArrayList;
 public class DiceHolder{
 
-	private ArrayList<Dice> holder;
+	private ArrayList<Die> holder = new ArrayList<>();
 
 	public DiceHolder(){
+
+	}
+
+	public int addDie(Die die){
+
+		Die a = new Die();
+		holder.add(a);
+
+		if (holder.size()<7)
+		return 1;
+		else
+		return -1;
+
 	}
 
 }
