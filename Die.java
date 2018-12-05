44bf34278668684556085948ecbcf97564eef44e
SS1618 <45392178+SS1618@users.noreply.github.com>
2018-12-05 08:29:44 -0500

    deleted spaces
    deleted extra lines in between code

--- Die.java
+++ Die.java
@@ -1,31 +1,21 @@
 public class Die{
-
 	private int side;
 	private int value;
-
 		public Die (){
 			side = 6;
 			value = 0;
 		}
-
 		public Die(int side, int value){
 			this.side = side;
 			this.value = value;
 		}
-
 		public int getValue(){
 			return value;
 		}
-
 		public void roll(){
 			this.value = (int)(Math.random()*side+1);
 		}
-
 		public String toString(){
 			return ("Number of sides: " + side + " & " + "Value: " + value);
 		}
-
-
-
-
 }
