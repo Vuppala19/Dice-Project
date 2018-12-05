a16e124ba4b5338e808e5b7f83d7bb2d63f9ff5c
SS1618 <45392178+SS1618@users.noreply.github.com>
2018-12-05 07:59:23 -0500

    Created Constructors
    created default and nondefault constructors for side and value

--- Die.java
+++ Die.java
@@ -1,3 +1,21 @@
 public class Die{
 
+	private int side;
+	private int value;
+
+		public Die (){
+			side = 6;
+			value = 0;
+		}
+
+		public Die(int side, int value){
+			this.side = side;
+			this.value = value;
+		}
+
+		//(int)(Math.random()*side)
+
+
+
+
 }
